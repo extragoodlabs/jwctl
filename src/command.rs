b2f4ecd29a6c36514c5dc8e57dab1990999fff4d
William Huba <william@jumpwire.io>
2023-08-02 10:49:22 -0400

    Remove redundant clone

--- src/command.rs
+++ src/command.rs
@@ -4,14 +4,14 @@ use serde_json::Value;
 
 /// Retrieve status information from the proxy server
 pub fn status(config: Config) -> Result<Value, reqwest::Error> {
-    let mut url = config.url.clone();
+    let mut url = config.url;
     url.set_path("/_jumpwire/status");
     reqwest::blocking::get(url)?.json()
 }
 
 /// Issue a ping command expecting to get back a pong
 pub fn ping(config: Config) -> Result<String, reqwest::Error> {
-    let mut url = config.url.clone();
+    let mut url = config.url;
     url.set_path("/_jumpwire/ping");
     reqwest::blocking::get(url)?.text()
 }
